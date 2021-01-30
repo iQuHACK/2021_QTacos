49cfffe3eb21ad4cafbb8282f32d32d495edc8f4
Diego Pérez <30667045+Diegoj029@users.noreply.github.com>
2021-01-30 16:34:38 -0600

    Cambios menores

--- pygame.py
+++ pygame.py
@@ -1,12 +1,14 @@
 import pygame, sys
 pygame.init()
 
-size = (800,500)
+size = (1280,720)
 
 screen = pygame.display.set_mode(size)
 
 while True:
 	for event in pygame.event.get():
 		print(event)
-		if event type == pygame.QUIT:
+		if event.type == pygame.QUIT:
 			sys.exit()
+
+	
