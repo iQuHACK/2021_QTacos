0169b89dd5f7e39104f26397868308a6258f6471
Diego Pérez <30667045+Diegoj029@users.noreply.github.com>
2021-01-31 09:20:26 -0600

    Removed image_list

--- game/game.py
+++ game/game.py
@@ -17,15 +17,13 @@ BLUE    = (     0,  0,  255)
 
 #Import resources
 game_folder = os.path.dirname(os.path.abspath(__file__))
-image_list = []
+
 game_bg = pg.image.load(os.path.join(game_folder,'resources/backgrounds/game.jpg'))
-image_list.append(game_bg)
+main_menu_bg = pg.image.load(os.path.join(game_folder,'resources/backgrounds/main_menu.jpg'))
 
 plate = pg.image.load(os.path.join(game_folder,'resources/images/plate.png'))
-image_list.append(plate)
 
 tortilla = pg.image.load(os.path.join(game_folder,'resources/images/tortilla.png'))
-image_list.append(tortilla)
 
 deshebrada = pg.image.load(os.path.join(game_folder,'resources/images/deshebrada.png'))
 trompo = pg.image.load(os.path.join(game_folder,'resources/images/trompo.png'))
@@ -33,7 +31,6 @@ pastor = pg.image.load(os.path.join(game_folder,'resources/images/pastor.png'))
 cilantro = pg.image.load(os.path.join(game_folder,'resources/images/cilantro.png'))
 cebolla = pg.image.load(os.path.join(game_folder,'resources/images/cebolla.png'))
 chicken = pg.image.load(os.path.join(game_folder,'resources/images/chicken.png'))
-image_list.extend([deshebrada,trompo,pastor,cilantro,cebolla,chicken])
 
 tortilla_glow = pg.image.load(os.path.join(game_folder,'resources/images/tortilla_glow.png'))
 deshebrada_glow = pg.image.load(os.path.join(game_folder,'resources/images/deshebrada_glow.png'))
@@ -41,14 +38,12 @@ trompo_glow = pg.image.load(os.path.join(game_folder,'resources/images/trompo_gl
 cilantro_glow = pg.image.load(os.path.join(game_folder,'resources/images/cilantro_glow.png'))
 cebolla_glow = pg.image.load(os.path.join(game_folder,'resources/images/cebolla_glow.png'))
 chicken_glow = pg.image.load(os.path.join(game_folder,'resources/images/chicken_glow.png'))
-image_list.extend([deshebrada_glow,trompo_glow,cilantro_glow,cebolla_glow,chicken_glow])
 
 paper = pg.image.load(os.path.join(game_folder,'resources/images/paper.png'))
 paper_glow = pg.image.load(os.path.join(game_folder,'resources/images/paper_glow.png'))
 canasta = pg.image.load(os.path.join(game_folder,'resources/images/canasta.png'))
 canasta_glow = pg.image.load(os.path.join(game_folder,'resources/images/canasta_glow.png'))
 no_glow = pg.image.load(os.path.join(game_folder,'resources/images/no_glow.png'))
-image_list.extend([paper,paper_glow,canasta,canasta_glow,no_glow])
 
 play = pg.image.load(os.path.join(game_folder,'resources/buttons/play.png'))
 play_glow = pg.image.load(os.path.join(game_folder,'resources/buttons/play_glow.png'))
@@ -60,7 +55,6 @@ options = pg.image.load(os.path.join(game_folder,'resources/buttons/options.png'
 options_glow = pg.image.load(os.path.join(game_folder,'resources/buttons/options_glow.png'))
 cred = pg.image.load(os.path.join(game_folder,'resources/buttons/credits.png'))
 cred_glow = pg.image.load(os.path.join(game_folder,'resources/buttons/credits_glow.png'))
-image_list.extend([play,play_glow,leaderboard,leaderboard_glow,howtoplay,howtoplay_glow,options,options_glow,cred,cred_glow])
 
 #Circuit and initial state
 qc, state = inicioRandom()
