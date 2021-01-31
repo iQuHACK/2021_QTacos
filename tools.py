ad32d113ad7ea22f5f6e30fe00724607a4f9c28a
Isaac Pimentel <isaacapimentelm@gmail.com>
2021-01-31 00:13:28 +0000

    tools actualizada
    se agredo la función randomQuantumState a tools

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-from qiskit import execute
+from qiskit import QuantumCircuit, Aer, execute
 
 def measuring(circuit, backend, shots=2000):
     job = execute(circuit, backend, shots=shots)
@@ -23,3 +23,22 @@ def add_gate(circuit, gate=None, channel=0, channel_op=0):
         circuit.cnot(channel, channel_op)
         
     return circuit
+
+def randomQuantumState():
+    qc = QuantumCircuit(3,3)
+    for i in range(0,int(random.randint(1,5))):
+        g = random.choice(("hadamard","x","y","z","cnot"))
+        if g == "cnot":
+            q = random.sample(tuple(range(0,3)),2)
+            add_gate(qc,gate=g,channel=q[0],channel_op=q[1])
+        else:
+            q = random.choice(tuple(range(0,3)))
+            add_gate(qc,gate=g,channel=q)
+
+    qc.barrier()
+    qc.measure([0,1,2],[0,1,2])
+    qc.draw('mpl')
+    
+    simulator = Aer.get_backend('qasm_simulator')
+    rqs = measuring(qc, backend=simulator)
+    return rqs
